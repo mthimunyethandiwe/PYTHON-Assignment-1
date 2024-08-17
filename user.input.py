c1a3edfffbfbf44e44215fe793f32d3e0350cfe3
mthimunyethandiwe <mthimunyethandiwe.m@gmail.com>
2024-08-17 15:50:19 +0200

    Add files via upload

---
+++ user.input.py
@@ -0,0 +1,74 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 11,
+   "id": "e49f7db2-2d71-4b22-a1cd-fef8c9e8405f",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "What is your name?:  Thandiwe\n",
+      "How old are you?:  29\n",
+      "Where do you live?:  Cape Town\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Hello Thandiwe\n",
+      "You are 29 years old\n",
+      "Where do you liveCape Town\n"
+     ]
+    }
+   ],
+   "source": [
+    "name = input(\"What is your name?: \")\n",
+    "age = int(input(\"How old are you?: \"))\n",
+    "location = input(\"Where do you live?: \")\n",
+    "\n",
+    "print(\"Hello \"+name)\n",
+    "print(\"You are \"+str(age)+\" years old\")\n",
+    "print(\"Where do you live\"+location)\n",
+    "\n",
+    "\n",
+    "\n",
+    "\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "52cc2990-ef21-42a8-bd87-92e2401ed54c",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "uygsj\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.11.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
